0e7401d991e1e2913e6459ac3dcf267f12de2e13
Fihri Aziz <fihriaziz@gmail.com>
2022-11-15 11:16:37 +0700

    initial commit

---
+++ app/Http/Controllers/auth/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $req)
+    {
+        try {
+            $user = User::where('email', $req->email)->first();
+            $token = $user->createToken()->plainTextToken;
+
+            $credentials = $req->only(['email', 'password']);
+            if (!$credentials) {
+                return response()->json([
+                    'message' => 'Unauthorized'
+                ], 403);
+            }
+
+            return response()->json([
+                'data' => $user,
+                'access_token' => $token,
+                'type' => 'Bearer'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json(['message:' . $e->getMessage()]);
+        }
+    }
+}
