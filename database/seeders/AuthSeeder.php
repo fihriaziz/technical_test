6feb244133a38b0416124aa533be97d385fcd49e
Fihri Aziz <fihriaziz@gmail.com>
2022-11-15 11:20:01 +0700

    create seeder auth

--- database/seeders/AuthSeeder.php
+++ database/seeders/AuthSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,10 @@ class AuthSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            'name' => 'fihri aziz',
+            'email' => 'fihriaziz@gmail.com',
+            'password' => bcrypt('password')
+        ]);
     }
 }
