097e594f3ba1cb706f83250a4789574da66f45d2
Fihri Aziz <fihriaziz@gmail.com>
2022-11-15 20:26:34 +0700

    endpoint forgot password with token

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/forgot-password', [AuthController::class, 'forgot'])->name('password.reset');
+Route::post('/forgot-password', [AuthController::class, 'forgot']);
+Route::get('/forgot-password/{token}', [AuthController::class, 'forgot_password'])->name('password.reset');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
